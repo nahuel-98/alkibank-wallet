283dda2d1d8c19ce7407e3980959179db21fcf79
mangiu90 <giudicimanuel@gmail.com>
2022-11-01 21:55:46 -0300

    Creo middleware de validacion

---
+++ middlewares/validator.js
@@ -0,0 +1,17 @@
+const { validationResult } = require('express-validator')
+
+const validate = (schemas)  => {
+  return async (req, res, next) => {
+    await Promise.all(schemas.map((schema) => schema.run(req)));
+
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      return next();
+    }
+
+    const errors = result.array();
+    return  res.send(errors)
+  };
+}
+
+module.exports = validate
