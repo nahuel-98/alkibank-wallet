0eea0432c361c55256067d0ad2e24ac1ab415ace
Pablo Oscar Chávez <89558154+PabloChavez03@users.noreply.github.com>
2022-11-02 18:21:51 -0300

    Model roles (#3)
    * feature(database):create model & migration role
    
    * remove example user per associate with role
    
    Co-authored-by: Manuel Giudici <68077160+mangiu90@users.noreply.github.com>

---
+++ database/models/role.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasMany(models.User, {
+        foreignKey:'roleId'
+      });
+    }
+  }
+  Role.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Role",
+    }
+  );
+  return Role;
+};
