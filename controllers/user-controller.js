124cb7f5223b802d50d04aba98c01efd1c671199
blasDelgado <blasdelgado10@gmail.com>
2022-11-03 16:23:22 -0300

    POST /users endpoint is created with its controller and userSchema

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,9 +2,12 @@ const createHttpError = require("http-errors");
 const { User } = require("../database/models");
 const { endpointResponse } = require("../helpers/success");
 const { catchAsync } = require("../helpers/catchAsync");
+const { ErrorObject } = require("../helpers/error");
+const { Security } = require("../config/security");
+const { response } = require("express");
 
 module.exports = {
-  allUser: catchAsync(async (req, res, next) => {
+  allUsers: catchAsync(async (req, res, next) => {
     try {
       const response = await User.findAll({
         attributes: ["firstname", "lastname", "email", "createdAt"],
@@ -28,6 +31,9 @@ module.exports = {
       const response = await User.findAll({
         where: { id: id },
       });
+      if (response[0] == null) {
+        throw new ErrorObject("ID provided not existing", 404);
+      }
       endpointResponse({
         res,
         message: "User retrieved successfully",
@@ -37,6 +43,36 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error retrieving /users/id] - [user - GET]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+  createUser: catchAsync(async (req, res, next) => {
+    try {
+      const { firstName, lastName, email, password } = req.body;
+      const encryptPassword = await Security.encryptPassword(password);
+      const isEmail = await User.findAll({
+        where: { email },
+      });
+      if (isEmail[0] != null) {
+        throw new ErrorObject("Email provided already existing", 409);
+      }
+      const response = await User.create({
+        firstName,
+        lastName,
+        email,
+        password: encryptPassword,
+      });
+      console.log(response);
+      endpointResponse({
+        res,
+        message: "User retrieved successfully",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving /users] - [user - POST]: ${error.message}`
       );
       next(httpError);
     }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { allUsers } = require("../controllers/user-controller");
+const {
+  allUsers,
+  idUser,
+  createUser,
+} = require("../controllers/user-controller");
+const validate = require("../middlewares/validator");
+const userSchema = require("../schemas/userSchema");
 
 const router = express.Router();
 
 router.get("/users", allUsers);
 
 router.get("/users/:id", idUser);
+
+router.post("/users", validate(userSchema), createUser);
 
 module.exports = router;
