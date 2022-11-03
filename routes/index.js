ad9cb636d2d9c4605fcaf34f46ed449bb5eaa5ec
PabloChavez03 <pablooscarchavez@gmail.com>
2022-11-03 14:28:04 -0300

    add routes by category

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const { get, post } = require('../controllers/index')
-const { validate } = require('../middlewares')
-const { testSchema } = require('../schemas')
+const express = require("express");
+const { get, post } = require("../controllers/index");
+const {
+  allCategories,
+  getCategory,
+  postCategory,
+  updateCategory,
+} = require("../controllers/category.controller");
+const { validate } = require("../middlewares");
+const { testSchema, categorySchema } = require("../schemas");
 
-const router = express.Router()
+const router = express.Router();
 
 // example of a route with index controller get function
-router.get('/', get)
+router.get("/", get);
 // example of a route with index controller post function
-router.post('/', validate(testSchema), post)
+router.post("/", validate(testSchema), post);
 
-module.exports = router
+//Crud - Categorias
+router.get("/category", allCategories);
+router.get("/category/:id", getCategory);
+router.post("/category", validate(categorySchema), postCategory);
+router.patch("/category/:id", validate(categorySchema), updateCategory);
+
+module.exports = router;
