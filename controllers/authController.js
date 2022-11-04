303b33c038c5511d0af8f574c2ccb65b0e4c63b6
mangiu90 <giudicimanuel@gmail.com>
2022-11-04 05:31:12 -0300

    Creo controlador de autenticacion

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const createHttpError = require('http-errors')
+const { User } = require('../database/models')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
+const { Security } = require('../config/security')
+
+module.exports = {
+    login: catchAsync(async (req, res, next) => {
+        try {
+            const { email, password } = req.body;
+
+            const user = await User.findOne({
+                where: { email }
+            });
+
+            if (!user) {
+                endpointResponse({
+                    res,
+                    code: 404,
+                    message: 'User does not exist',
+                    body: { ok: false },
+                });
+                return;
+            }
+
+            if (!await Security.compare(password, user.password)) {
+                endpointResponse({
+                    res,
+                    code: 403,
+                    message: 'Invalid password',
+                    body: { ok: false },
+                });
+                return;
+            }
+
+            endpointResponse({
+                res,
+                message: 'User login successfully',
+                body: { ok: true },
+            });
+            return;
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error login authentication] - [auth/login - POST]: ${error.message}`,
+            )
+            next(httpError)
+        }
+    }),
+}
