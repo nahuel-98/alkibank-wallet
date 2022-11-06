b6239caf1237a5b9ac5757e5c0927c9c95bd31f2
mangiu90 <giudicimanuel@gmail.com>
2022-11-06 17:37:27 -0300

    Agrego relacion de categoria con transacciones

--- database/models/category.js
+++ database/models/category.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // Category.hasMany(models.Transaction)
+      this.hasMany(models.Transaction)
     }
   };
   Category.init({
