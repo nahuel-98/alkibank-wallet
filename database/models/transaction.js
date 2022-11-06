ab8daa1f2522a8354aa9c4650b048469ca4d2cc8
mangiu90 <giudicimanuel@gmail.com>
2022-11-06 17:36:55 -0300

    Creo modelo de transacciones

---
+++ database/models/transaction.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Category)
+    }
+  };
+  Transaction.init({
+    description: DataTypes.STRING,
+    amount: DataTypes.DECIMAL,
+    userId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+    date: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'Transaction',
+    paranoid: true,
+  });
+  return Transaction;
+};
