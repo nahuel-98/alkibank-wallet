3b6067020e5de85b684682dd7e3cc85e4e2e3744
Nahuel Medina <nahuelmedia@hotmail.com>
2022-11-06 22:07:05 -0300

    agregar controladores de images

--- app.js
+++ app.js
@@ -12,13 +12,32 @@ const indexRouter = require('./routes/index')
 const port = process.env.PORT || 3000
 
 const app = express()
+
+
 app.use(cors())
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+//multer, service for uploading images
+app.use(multer({
+  dest: path.join(__dirname, 'public/uploads'),
+  fileFilter: function (req, file, cb) {
+
+      var filetypes = /svg|jpg|png|webp/;
+      var mimetype = filetypes.test(file.mimetype);
+      var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+      if (mimetype && extname) {
+          return cb(null, true);
+      }
+      cb("Error: File upload only supports the following filetypes - " + filetypes);
+  },
+  limits: {fileSize: 2000000},
+}).single('image'));
 
 app.use('/', indexRouter)
 
@@ -38,22 +57,7 @@ app.use((err, req, res) => {
   res.render('error')
 })
 
-//multer, service for uploading images
-app.use(multer({
-  dest: path.join(__dirname, 'public/uploads'),
-  fileFilter: function (req, file, cb) {
 
-      var filetypes = /svg|jpg|png|webp/;
-      var mimetype = filetypes.test(file.mimetype);
-      var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-      if (mimetype && extname) {
-          return cb(null, true);
-      }
-      cb("Error: File upload only supports the following filetypes - " + filetypes);
-  },
-  limits: {fileSize: 2000000},
-}).single('image'));
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
--- routes/images.routes.js
+++ routes/images.routes.js
@@ -1,30 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
-const multer = require('multer');
-const fs = require('fs');
+const {uploadImages} = require('../controllers/image.controller')
 
-
-const storage = multer.diskStorage({
-    destination: path.join(__dirname, '../public/uploads'),
-    filename:  (req, file, cb) => {
-        cb(null, file.originalname);
-    }
-})
-const uploadImage = multer({
-    storage,
-    limits: {fileSize: 1000000}
-}).single('image');
-
-router.post('/upload', (req, res) => {
-    uploadImage(req, res, (err) => {
-        if (err) {
-            err.message = 'The file is so heavy for my service';
-            return res.send(err);
-        }
-        console.log(req.file);
-        res.send('uploaded');
-    });
-});
+router.post('/upload', uploadImages);
 
 module.exports = router;
