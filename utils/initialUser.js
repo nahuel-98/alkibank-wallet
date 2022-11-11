d01fbc3e7d6067dcacff82498031b9799ca0f00e
PabloChavez03 <pablooscarchavez@gmail.com>
2022-11-11 00:56:46 -0300

    initialUser for createUser

---
+++ utils/initialUser.js
@@ -0,0 +1,18 @@
+const initialUser = [
+  {
+    id: 1,
+    firstName: "Pablo Oscar",
+    lastName: "Chavez",
+    email: "pablooscarchavez@gmail.com",
+    password: "prueba123",
+  },
+  {
+    id: 2,
+    firstName: "Eduardo",
+    lastName: "Garcia",
+    email: "edu_la_acade@gmail.com",
+    password: "prueba123",
+  },
+];
+
+module.exports = initialUser;
