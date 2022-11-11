8886c4deab42892eba268d00c0c776d556773fcd
PabloChavez03 <pablooscarchavez@gmail.com>
2022-11-11 12:13:23 -0300

    rm user innecesary

--- utils/initialUser.js
+++ utils/initialUser.js
@@ -5,14 +5,7 @@ const initialUser = [
     lastName: "Chavez",
     email: "pablooscarchavez@gmail.com",
     password: "prueba123",
-  },
-  {
-    id: 2,
-    firstName: "Eduardo",
-    lastName: "Garcia",
-    email: "edu_la_acade@gmail.com",
-    password: "prueba123",
-  },
+  }
 ];
 
 module.exports = initialUser;
